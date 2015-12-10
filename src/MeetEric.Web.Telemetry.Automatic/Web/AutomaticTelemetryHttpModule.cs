00a7d7b463fffb6bc154b8f9752ef68115b9575c
Eric Maino <ericmai@microsoft.com>
2015-12-09 17:24:54 -0800

    Initial project

---
+++ src/MeetEric.Web.Telemetry.Automatic/Web/AutomaticTelemetryHttpModule.cs
@@ -0,0 +1,30 @@
+﻿using MeetEric.Properties;
+using Microsoft.ApplicationInsights.Web;
+using System;
+using System.IO;
+using System.Text;
+using System.Web;
+
+namespace MeetEric.Web
+{
+    public class AutomaticTelemetryHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            DeployConfigFile();
+            new ApplicationInsightsHttpModule().Init(context);
+        }
+
+        private void DeployConfigFile()
+        {
+            // HACK: This should go away soon once application insights doesn't 
+            // require reading configuration from a specific file on disk
+
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ApplicationInsights.config"), Resources.ApplicationInsights, Encoding.UTF8);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/MeetEric.Web.Telemetry.Automatic/Web/HttpApplicationWithTelemetry.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace MeetEric.Web
+{
+    public class HttpApplicationWithTelemetry : HttpApplication
+    {
+        public override void Init()
+        {
+            base.Init();
+            new AutomaticTelemetryHttpModule().Init(this);
+        }
+    }
+}
